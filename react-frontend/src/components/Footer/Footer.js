daa7e38a61be226903952d92994e0092460f8318
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-07 23:56:17 -0400

    Add links to social icons

--- react-frontend/src/components/Footer/Footer.js
+++ react-frontend/src/components/Footer/Footer.js
@@ -7,22 +7,30 @@ class Footer extends Component {
       <footer className="footer" style={{ marginTop: "150px" }}>
         <MDBNavbar className="blue-gradient" expand="md" style={{ color: "white" }} scrolling fixed="bottom">
           <MDBNavbarBrand>
-            <p className="mt-3" style={{ fontSize: '16px' }}>
+            <p className="mt-3 ml-3" style={{ fontSize: '16px' }}>
               &copy; {new Date().getFullYear()} Copyright: Sasha Obucina
             </p>
           </MDBNavbarBrand>
           <MDBNavbarNav right>
             <MDBNavItem className="mr-3 ml-3">
-              <MDBIcon className="white-text" fab icon="google" size="lg"/>
+              <a className="hoverable" href="mailto:sasha.obucina@gmail.com">
+                <MDBIcon className="white-text" fab icon="google" size="lg"/>
+              </a>
             </MDBNavItem>
             <MDBNavItem className="mr-3 ml-3">
-              <MDBIcon className="white-text" fab icon="github" size="lg"/>
+              <a className="hoverable" target="_blank" href="https://github.com/sashaobucina">
+                <MDBIcon className="white-text" fab icon="github" size="lg"/>
+              </a>
             </MDBNavItem>
             <MDBNavItem className="mr-3 ml-3">
-              <MDBIcon className="white-text" fab icon="linkedin-in" size="lg"/>
+              <a className="hoverable" target="_blank" href="https://www.linkedin.com/in/sasha-obucina-0b51a2173/">
+                <MDBIcon className="white-text" fab icon="linkedin-in" size="lg"/>
+              </a>
             </MDBNavItem>
             <MDBNavItem className="mr-3 ml-3">
-              <MDBIcon className="white-text" far icon="user" size="lg"/>
+              <a className="hoverable" href="">
+                <MDBIcon className="white-text" far icon="user" size="lg"/>
+              </a>
             </MDBNavItem>
           </MDBNavbarNav>
         </MDBNavbar>
