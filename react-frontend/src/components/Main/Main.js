507ba637649f0af8eebeb73e9ffc2b328c6c2cfd
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-08 16:02:13 -0400

    Remove unused imports

--- react-frontend/src/components/Main/Main.js
+++ react-frontend/src/components/Main/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
--- react-frontend/src/components/SupportedSymbols/SupportedSymbols.js
+++ react-frontend/src/components/SupportedSymbols/SupportedSymbols.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { MDBDataTable, MDBIcon } from "mdbreact";
+import { MDBDataTable } from "mdbreact";
 
 class SupportedSymbols extends Component {
   constructor(props) {
