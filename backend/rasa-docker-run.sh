5c80f2bc43ecc52a497eb0f0e82fa08e2e59fa34
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-08 23:31:03 -0400

    Change up names and relative paths in backend

--- backend/rasa-docker-run.sh
+++ backend/rasa-docker-run.sh
@@ -1,6 +1,6 @@
 docker run \
   --rm -p 5000:5000 \
-  -v $HOME/repos/web_apps/statement_extractor/rasa_model:/app/projects \
+  -v $HOME/repos/web_apps/financebuddy/rasa_model:/app/projects \
   rasa/rasa_nlu:stable-full \
   start \
     --path /app/projects \
