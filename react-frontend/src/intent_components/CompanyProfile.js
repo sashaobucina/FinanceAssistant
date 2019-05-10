726559f60f65dba19cc1c8d9fe8a1074c5ffda57
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-10 14:20:18 -0400

    Account for company rating in CompanyProfile intent (frontend)

--- react-frontend/src/intent_components/CompanyProfile.js
+++ react-frontend/src/intent_components/CompanyProfile.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 
 class CompanyProfile extends Component {
   render() {
-    const { profile, ticker} = this.props.data
+    const { profile, rating, ticker} = this.props.data
     return (
       <div>
         <p>Company profile for {ticker.companyName}</p>
         {JSON.stringify(profile)}
+        {JSON.stringify(rating)}
       </div>
     )
   }
