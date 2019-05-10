5e301425225d3e77328ff645bb8f1d11c6681b30
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-10 14:44:48 -0400

    Add message that FinanceAssistant supports crypto information

--- react-frontend/src/components/About/About.js
+++ react-frontend/src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
+import { MDBIcon } from "mdbreact";
 
 class About extends Component {
   render() {
@@ -11,6 +12,13 @@ class About extends Component {
           </div>
           <div className="about-chat-history">
             <p>Use the '<samp>&uarr; &darr;</samp>' keys to navigate quickly through your previous messages!</p>
+          </div>
+          <div className="about-cryptos">
+            <p>
+              Now supporting cryptocurrency information!
+              <MDBIcon className="ml-2 amber-text" fab icon="bitcoin" />
+              <MDBIcon className="ml-2" fab icon="ethereum" />
+            </p>
           </div>
         </Col>
       </Row>
