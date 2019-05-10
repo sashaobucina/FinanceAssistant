7b96659407c0a194b5c7239f7c164474cd7edb59
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-10 19:13:41 -0400

    Add rounded border to error response w/ invalid tickers

--- react-frontend/src/intent_components/InvalidTicker.js
+++ react-frontend/src/intent_components/InvalidTicker.js
@@ -7,17 +7,22 @@ class InvalidTicker extends Component {
     const { error } = this.props.data;
     return (
       <div className="invalid-ticker">
-        <h5 className="text-muted mb-3 font-weight-bold">
-          {error}
-        </h5>
-        <p className="text-muted mb-4">
-          Refer to the <Link to="/symbols">Valid Symbols</Link> page to get a list of all the valid symbols
-        </p>
-        <Link to="/symbols">
-          <MDBBtn className="blue-gradient ml-4">
-            Symbols<MDBIcon className="ml-2" icon="chart-line"></MDBIcon>
-          </MDBBtn>
-        </Link>
+        <div className="invalid-ticker-icon">
+          <MDBIcon className="red-text mb-3" icon="exclamation-triangle" size="5x" />
+        </div>
+        <div className="border rounded-pill">
+          <h5 className="text-muted mb-3 mt-4 font-weight-bold">
+            {error}
+          </h5>
+          <p className="text-muted mb-4">
+            Refer to the <Link to="/symbols">Valid Symbols</Link> page to get a list of all the valid symbols
+          </p>
+          <Link to="/symbols">
+            <MDBBtn className="blue-gradient ml-4 mb-3">
+              Symbols<MDBIcon className="ml-2" icon="chart-line"></MDBIcon>
+            </MDBBtn>
+          </Link>
+        </div>
       </div>
     )
   }
