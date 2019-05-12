6bd7337c8598c893fa180737d0b6bf85fe5606f1
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-12 19:42:51 -0400

    Fix navbar hamburger animation bug - made static

--- react-frontend/src/components/Header/Header.js
+++ react-frontend/src/components/Header/Header.js
@@ -1,22 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { MDBNavbar, MDBNavbarBrand, MDBHamburgerToggler, MDBCollapse, MDBNavItem, MDBNavbarNav, MDBIcon } from "mdbreact";
+import { MDBNavbar, MDBNavbarBrand, MDBCollapse, MDBNavItem, MDBNavbarNav, MDBIcon, MDBNavbarToggler } from "mdbreact";
+import hamburgerIcon from "../../static/images/hamburger-svg.png"
 
 class Header extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      collapse1: false,
       collapseID: ''
     }
-    this.toggleSingleCollapse = this.toggleSingleCollapse.bind(this);
   }
 
-  toggleSingleCollapse(collapseId) {
-    this.setState({
-      ...this.state,
-      [collapseId]: !this.state[collapseId]
-    })
+  toggleCollapse = (collapseId) => () => {
+    this.setState(prevState => ({ collapseID: (prevState.collapseID !== collapseId ? collapseId : '') }))
   }
 
   render() {
@@ -28,31 +24,31 @@ class Header extends Component {
               <strong>Finance Assistant<MDBIcon className="ml-1" icon="hand-holding-usd" /></strong>
             </Link>
           </MDBNavbarBrand>
-          <MDBHamburgerToggler className="mr-2" id="hamburger1" onClick={() => this.toggleSingleCollapse('collapse1')} />
-            <MDBCollapse isOpen={this.state.collapse1} navbar>
-              <MDBNavbarNav style={{ textAlign: "left", marginTop: "15px" }}>
-                <MDBNavItem active>
-                  <Link to="/" onClick={() => this.toggleSingleCollapse('collapse1')}>
-                    <p><MDBIcon className="mr-2" icon="home" />Home</p>
-                  </Link>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <Link to="/symbols" onClick={() => this.toggleSingleCollapse("collapse1")}>
-                    <p><MDBIcon className="mr-2" icon="chart-line" />Valid Symbols</p>
-                  </Link>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <Link to="/about" onClick={() => this.toggleSingleCollapse('collapse1')}>
-                    <p><MDBIcon className="mr-2" icon="question" />About Us</p>
-                  </Link>
-                </MDBNavItem>
-                <MDBNavItem>
-                  <Link to="/contact" onClick={() => this.toggleSingleCollapse('collapse1')}>
-                    <p><MDBIcon className="mr-2" icon="phone" />Contact</p>
-                  </Link>
-                </MDBNavItem>
-              </MDBNavbarNav>
-            </MDBCollapse>
+          <MDBNavbarToggler image={hamburgerIcon} className="mr-2" id="hamburger1" onClick={this.toggleCollapse('navbarCollapse1')} />
+          <MDBCollapse id="navbarCollapse1" isOpen={this.state.collapseID} navbar>
+            <MDBNavbarNav style={{ textAlign: "left", marginTop: "15px" }}>
+              <MDBNavItem active>
+                <Link to="/" onClick={this.toggleCollapse('navbarCollapse1')}>
+                  <p><MDBIcon className="mr-2" icon="home" />Home</p>
+                </Link>
+              </MDBNavItem>
+              <MDBNavItem>
+                <Link to="/symbols" onClick={this.toggleCollapse("navbarCollapse1")}>
+                  <p><MDBIcon className="mr-2" icon="chart-line" />Valid Symbols</p>
+                </Link>
+              </MDBNavItem>
+              <MDBNavItem>
+                <Link to="/about" onClick={this.toggleCollapse('navbarCollapse1')}>
+                  <p><MDBIcon className="mr-2" icon="question" />About Us</p>
+                </Link>
+              </MDBNavItem>
+              <MDBNavItem>
+                <Link to="/contact" onClick={this.toggleCollapse('navbarCollapse1')}>
+                  <p><MDBIcon className="mr-2" icon="phone" />Contact</p>
+                </Link>
+              </MDBNavItem>
+            </MDBNavbarNav>
+          </MDBCollapse>
         </MDBNavbar>
       </header>
     )
