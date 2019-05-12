293eb17b71783dc0488cb51ff788ef80aa493ddd
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-11 20:16:49 -0400

    Add error handling for financial statement component

--- react-frontend/src/intent_components/FInancialStatement.js
+++ react-frontend/src/intent_components/FInancialStatement.js
@@ -6,41 +6,49 @@ import { MDBDataTable, MDBRow, MDBCol } from "mdbreact";
 class FinancialStatement extends Component {
   render() {
     const { csv, financials, ticker } = this.props.data;
-    const entry = financials[0];
-    const columns = Object.keys(entry).map(key => ({
-      label: key,
-      field: key.toLowerCase()
-    }))
-    const rows = this.formatNumbers(financials);
-    const data = { columns, rows };
+    let data = [];
+    if (financials.length !== 0) {
+      const entry = financials[0];
+      const columns = Object.keys(entry).map(key => ({
+        label: key,
+        field: key.toLowerCase()
+      }))
+      const rows = this.formatNumbers(financials);
+      data = { columns, rows };
+    }
     return (
-      <>
-        <MDBRow>
-          <MDBCol md="3" style={{ textAlign: "left" }}>
-            <DownloadButton csv={csv} filename={`AnnualCashFlow_${ticker.symbol}`} />
-          </MDBCol>
-          <MDBCol md="6">
-            <h5 className="mt-4 financial-statement-title text-muted">
-              {this.props.title} for {ticker.companyName} (in millions)
-            </h5>
-          </MDBCol>
-        </MDBRow>
-        <MDBRow>
-          <MDBCol md="12">
-            <div className="annual-cash-flow">
-              <MDBDataTable
-                bordered
-                striped
-                info={false}
-                paging={false}
-                sortable={false}
-                theadColor="blue-gradient"
-                data={data}
-              />
-            </div>
-          </MDBCol>
-        </MDBRow>
-      </>
+      <div className="financial-statment">
+        { financials.length === 0
+            ? <h4 className="text-muted">No information for {ticker.companyName}</h4>
+            : <>
+                <MDBRow>
+                  <MDBCol md="3" style={{ textAlign: "left" }}>
+                    <DownloadButton csv={csv} filename={`AnnualCashFlow_${ticker.symbol}`} />
+                  </MDBCol>
+                  <MDBCol md="6">
+                    <h5 className="mt-4 financial-statement-title text-muted">
+                      {this.props.title} for {ticker.companyName} (in millions)
+                    </h5>
+                  </MDBCol>
+                </MDBRow>
+                <MDBRow>
+                  <MDBCol md="12">
+                    <div className="annual-cash-flow">
+                      <MDBDataTable
+                        bordered
+                        striped
+                        info={false}
+                        paging={false}
+                        sortable={false}
+                        theadColor="blue-gradient"
+                        data={data}
+                      />
+                    </div>
+                  </MDBCol>
+                </MDBRow>
+              </>
+        }
+        </div>
     )
   }
 
