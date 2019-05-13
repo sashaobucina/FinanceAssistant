49d4520dbce2cc2681bf0a72875bcd03019730c8
Sasha Obucina <sasha.obucina@gmail.com>
2019-05-12 22:24:00 -0400

    Add support for company profile component (75% done)

--- react-frontend/src/helper.js
+++ react-frontend/src/helper.js
@@ -9,6 +9,19 @@ export const isNumber = (val) => {
   return !isNaN(parseFloat(val) && !isNaN(val - 0));
 }
 
+// stack overflow -> https://stackoverflow.com/questions/9461621/format-a-number-as-2-5k-if-a-thousand-or-more-otherwise-900
+export const nFormatter = (num) => {
+  if (num >= 1000000000) {
+    return (num/1000000000).toFixed(2) + 'B';
+  } else if (num >= 1000000) {
+    return (num/1000000).toFixed(2) + 'M';
+  } else if (num >= 1000) {
+    return (num/1000).toFixed(2) + 'K'
+  } else {
+    return String(num);
+  }
+}
+
 export const getMinMax = (historicalPrices, key) => {
   const values = historicalPrices.map(price => price[key]);
   const low = key === 'date' ? values[0] : Math.min(...values);
--- react-frontend/src/intent_components/CompanyProfile.js
+++ react-frontend/src/intent_components/CompanyProfile.js
@@ -1,16 +1,89 @@
 import React, { Component } from "react";
+import { MDBRow, MDBContainer, MDBCol, MDBIcon, MDBListGroup, MDBListGroupItem } from "mdbreact";
+import { nFormatter } from "../helper";
 
 class CompanyProfile extends Component {
   render() {
-    const { profile, rating, ticker} = this.props.data
+    const { profile, rating, ticker } = this.props.data
+    const { companyName, symbol } = ticker;
     return (
-      <div>
-        <p>Company profile for {ticker.companyName}</p>
+      <div className="company-profile">
+        <MDBContainer>
+          <MDBRow>
+            <MDBCol className="border rounded" md="12">
+              <MDBRow className="mt-2">
+                <MDBCol md="12">
+                  <h4 className="">{companyName}</h4>
+                  <img src={`https://financialmodelingprep.com/images-New-jpg/${symbol}.jpg`} alt="comapanyLogo" className="img-thumbnail" />
+                </MDBCol>
+              </MDBRow>
+              <MDBRow className="mb-2 mt-3">
+                <MDBCol md="4">
+                  <p className="text-muted">Company Rating:</p>
+                  <div className="rating-stars">{showCompanyRating(rating)}</div>
+                </MDBCol>
+                <MDBCol md="4">
+                  <p className="text-muted">Stock Price:</p>
+                  <div className="profile-stock-price">{showStockPrice(profile)}</div>
+                </MDBCol>
+                <MDBCol md="4">
+                  <p className="text-muted">Symbol:</p>
+                  <h3>{symbol}</h3>
+                </MDBCol>
+              </MDBRow>
+              <MDBRow className="mb-3">
+                <MDBCol className="offset-md-1" md="10" style={{ textAlign: "left" }}>
+                  {showOverview(profile)}
+                </MDBCol>
+              </MDBRow>
+            </MDBCol>
+          </MDBRow>
+          <MDBRow>
+            <MDBCol md="12">
+              
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
         {JSON.stringify(profile)}
-        {JSON.stringify(rating)}
       </div>
     )
   }
+}
+
+export const showOverview = (companyProfile) => {
+  const { Beta, VolAvg, MktCap, LastDiv } = companyProfile;
+  return (
+    <MDBListGroup>
+      <MDBListGroupItem><span className="text-muted">Beta:</span> <b>{Number(Beta).toFixed(2)}</b></MDBListGroupItem>
+      <MDBListGroupItem><span className="text-muted">Vol Average:</span> <b>{`$${nFormatter(VolAvg)}`}</b></MDBListGroupItem>
+      <MDBListGroupItem><span className="text-muted">Market Cap:</span> <b>{`$${nFormatter(MktCap)}`}</b></MDBListGroupItem>
+      <MDBListGroupItem><span className="text-muted">Last Dividend:</span> <b>{LastDiv !== "0" ? LastDiv : "N/A"}</b></MDBListGroupItem>
+    </MDBListGroup>
+  )
+}
+
+export const showCompanyRating = (companyRating) => {
+  const rating = companyRating["rating"];
+  return [...Array(5)].map((_e, i) => {
+    const color = (i < rating) ? "amber-text" : "grey-text";
+    return <MDBIcon key={i} className={color} icon="star" />
+  });
+}
+
+export const showStockPrice = (companyProfile) => {
+  const { Changes, ChangesPerc, Price } = companyProfile;
+  const color = Changes >= 0 ? "green-text": "red-text";
+  const changeIcon = (Changes >= 0) ? <MDBIcon icon="caret-up" className={`ml-2 ${color}`} /> : <MDBIcon icon="caret-down" className={color} />;
+  return (
+    <>
+      <h5>
+        {`$${Number(Price).toFixed(2)}`}
+        {changeIcon}
+      </h5>
+      <p className={color}>{Changes} {ChangesPerc}</p>
+      <p className={color}></p>
+    </>
+  )
 }
 
 export default CompanyProfile;
